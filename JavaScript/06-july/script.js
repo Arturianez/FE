6d50076dac024f542b85523e4264c6487c06f3be
Arturianez <artur.zank@gmail.com>
2023-07-09 19:48:22 -0400

    JavaScript #1

---
+++ JavaScript/06-july/script.js
@@ -0,0 +1,39 @@
+// const number = prompt("Введите число")
+// const result = number / 10
+// console.log(result)
+
+
+// const number1 = prompt("Веедите первое число")
+// const number2 = prompt("Веедите второе число")
+// if (number1 > number2) {
+//     console.log("Наименьшее число: " + number2)
+// } else {
+//     console.log("Наименьшее число: " + number1)
+// }
+
+
+// const number = prompt("Введите число ")
+// if ( number > 100) {
+//     console.log('Число больше 100')
+// } else if ( number == 100) {
+//     console.log('Число равно 100')
+// } else {
+//     console.log("Число меньше 100")
+// }
+
+// const name = prompt("Введите своё имя")
+// const age = prompt("Введите ваш возраст")
+// if (age > 21) {
+//     console.log("Hello " + name)
+// } else {
+//     console.log("Hi " + name)
+// }
+
+
+const name = prompt("Введите ваше имя")
+const lastName = prompt("Введите вашу фамилию")
+const age = prompt("Введите ваш возраст")
+const city = prompt("Введите город в котором проживаете")
+const animal = prompt("Введите имя вашего домашнего питомца")
+console.log(`Меня зовут ${name} ${lastName}, мне ${age}, я живу в ${city}, у меня есть животное по имени ${animal} `)
+
