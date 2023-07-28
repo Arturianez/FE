b4920ca996574c6c7650b5264ad8d2426a123c70
Arturianez <artur.zank@gmail.com>
2023-07-27 22:55:24 -0400

    HM#6

---
+++ JavaScript/Number_6/script.js
@@ -0,0 +1,25 @@
+
+// for (i=0; i <= 10; i += 2) {
+//     console.log(i)
+// }
+
+// for (let i = 55; i >= 21; i--) {
+//     console.log(i)
+// }
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+// console.log(numbers)
+// const numbers_squared = numbers.map(num => num ** 2)
+// console.log(numbers_squared)
+// const last_elem = numbers_squared[6]
+// console.log(last_elem)
+
+const user = {
+    first_name: 'Ivan',
+    last_name: 'Ivanov',
+    age: 20,
+    salary: 500
+}
+console.log("User's name is " + user.first_name + " " + user.last_name + ". He is " + user.age + " years old")
+// в последним задании мне кажется что можно сделать короче запись. Возможно путаю с вызовом и функции, но если есть какой комментарий, дай пожалуйста обратную связь
+
