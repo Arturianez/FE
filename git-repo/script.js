f127233e1c6832699b6323f9185d33aaf623dd30
Arturianez <artur.zank@gmail.com>
2023-08-19 13:08:43 -0400

    update

--- git-repo/script.js
+++ git-repo/script.js
@@ -1,3 +1,8 @@
 const wrapper = document.querySelector('.wrapper')
 console.log(wrapper)
 const newElem = document.createElement('div')
+
+// git add . - добавить все изменения
+// git status - проверить статус изменений
+// git commit -m ‘ваш первый коммит’
+// Git push origin main (вместо Рейн может быть название другой ветки)
